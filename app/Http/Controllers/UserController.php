0c8ff3a49aa0769961f15b5c0f703528ea29fe4c
Abdul Moeez <abdulmoeez1225@gmail.com>
2021-08-06 23:17:59 +0500

    Email Verification

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
     public function index()
     {
         $user = User::all();
-        return "abc";
+        return new UserResource($user);
     }
 
     /**
